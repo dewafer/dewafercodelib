2746fee6330a784b835584e7dbb0c5d1bafbb133
dewafer <dewafer@ec9725b6-cf5f-11dd-911d-6bf5514d214c>
2008-12-21 14:26:29 +0000

    hello world is the first step

---
+++ trunk/javacode/src/dewafer/java/util/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package dewafer.java.util;
+
+/**
+ * @author 
+ *
+ */
+public final class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+	}
+
+}
