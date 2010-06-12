6944ef6f09eea2c469f0e97dd05bcdfcc071b015
dewafer <dewafer@ec9725b6-cf5f-11dd-911d-6bf5514d214c>
2010-06-12 12:07:39 +0000

    first invade

---
+++ BackWordGUI/src/backwordgui/BackWordGUIApp.java
@@ -0,0 +1,44 @@
+/*
+ * BackWordGUIApp.java
+ */
+
+package backwordgui;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class BackWordGUIApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new BackWordGUIView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of BackWordGUIApp
+     */
+    public static BackWordGUIApp getApplication() {
+        return Application.getInstance(BackWordGUIApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(BackWordGUIApp.class, args);
+    }
+}
